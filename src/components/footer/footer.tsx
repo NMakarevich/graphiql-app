4b89a1411d7b792b8c58aea497c8c25f3bf6a893
NMakarevich <nikoladze529@mail.ru>
2024-08-29 22:49:45 +0300

    feat: add footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,57 @@
+import { FC } from 'react';
+import styles from './footer.module.scss';
+import { Container } from '@components/Container/Container.tsx';
+import { Link, Toolbar } from '@mui/material';
+import Contributor from '@components/contributor/contributor.tsx';
+import Image from 'next/image';
+
+export interface IContributor {
+  name: string;
+  url: string;
+}
+
+const contributors: IContributor[] = [
+  {
+    name: 'Tatyana Antipova',
+    url: 'https://github.com/L1senochek',
+  },
+  {
+    name: 'Maxim Ravinskiy',
+    url: 'https://github.com/GreyAdmiral',
+  },
+  {
+    name: 'Nikolay Makarevich',
+    url: 'https://github.com/NMakarevich',
+  },
+];
+
+const Footer: FC = () => {
+  return (
+    <footer className={styles.Footer}>
+      <Container>
+        <Toolbar color={'primary'} className={styles.Toolbar}>
+          <div className={styles.Contributors}>
+            {contributors.map((contributor, index) => (
+              <Contributor key={index} contributor={contributor} />
+            ))}
+          </div>
+          <div className={styles.Team}>2024</div>
+          <div className={styles.Course}>
+            <Link href={'https://rs.school/react/'}>
+              <Image
+                className={styles.Image}
+                width={32}
+                height={32}
+                src={'/rss-logo.svg'}
+                alt={'RSS Logo'}
+                unoptimized
+              />
+            </Link>
+          </div>
+        </Toolbar>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
