390edbe1c5b6a7af78a747fac19969dc82aa7c67
Binarion <itmaster@duck.com>
2024-08-29 21:31:39 +0300

    feat: 01-06_01_add_modals_and custom_hooks

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,79 @@
+'use client';
+import {
+  FC,
+  PropsWithChildren,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+} from 'react';
+import { createPortal } from 'react-dom';
+import { useClickOutside } from '@hooks/useClickOutside';
+import { useLockScroll } from '@hooks/useLockScroll';
+import styles from './Modal.module.scss';
+
+interface IModalProps {
+  isOpenModal: boolean;
+  onClose: () => void;
+}
+
+const modalCloseKeysCodes = ['Escape'];
+
+export const Modal: FC<PropsWithChildren & IModalProps> = ({
+  isOpenModal,
+  onClose,
+  children,
+  ...props
+}) => {
+  const modalRef = useRef(null);
+  const modalRoot = useMemo(() => {
+    const element = document.createElement('div');
+
+    element.classList.add(styles.modal);
+    return element;
+  }, []);
+  const keydownHandler = useCallback(
+    (e: KeyboardEvent) => {
+      e.stopPropagation();
+
+      if (modalCloseKeysCodes.includes(e.code)) {
+        onClose();
+      }
+    },
+    [onClose]
+  );
+
+  useClickOutside(modalRef, onClose);
+  useLockScroll(isOpenModal);
+
+  useEffect(() => {
+    if (isOpenModal) {
+      document.body.append(modalRoot);
+      document.body.addEventListener('keydown', keydownHandler);
+    }
+
+    return () => {
+      if (isOpenModal) {
+        modalRoot.remove();
+        document.body.removeEventListener('keydown', keydownHandler);
+      }
+    };
+  });
+
+  if (isOpenModal) {
+    return createPortal(
+      <div
+        className={styles.modalBody}
+        ref={modalRef}
+        role="dialog"
+        aria-modal={isOpenModal}
+        {...props}
+      >
+        {children}
+      </div>,
+      modalRoot
+    );
+  }
+
+  return null;
+};
