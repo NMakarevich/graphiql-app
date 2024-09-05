61ea25aff89c53edbdc6eef244eef3d9a3705173
NMakarevich <nikoladze529@mail.ru>
2024-09-05 21:20:42 +0300

    refactor: update auth control according main page requirements

--- src/components/authControl/authControl.tsx
+++ src/components/authControl/authControl.tsx
@@ -3,6 +3,7 @@ import Button from '@mui/material/Button';
 import { IAuthProps } from './types';
 import { useRouter } from 'next/navigation';
 import { ROUTES } from '@/utils/constants/routes.ts';
+import styles from './authControl.module.scss';
 
 const AuthControl: FC<IAuthProps> = (props): JSX.Element => {
   const { isAuth } = props;
@@ -14,14 +15,23 @@ const AuthControl: FC<IAuthProps> = (props): JSX.Element => {
     router.push(ROUTES.SIGN_IN_PATH);
   }
 
+  function signUp() {
+    router.push(ROUTES.SIGN_UP_PATH);
+  }
+
   return isAuth ? (
     <Button variant="outlined" onClick={signOut}>
       Sign out
     </Button>
   ) : (
-    <Button variant="contained" onClick={signIn}>
-      Sign in
-    </Button>
+    <div className={styles.Auth}>
+      <Button variant="contained" onClick={signIn}>
+        Sign in
+      </Button>
+      <Button variant="contained" onClick={signUp}>
+        Sign Up
+      </Button>
+    </div>
   );
 };
 
