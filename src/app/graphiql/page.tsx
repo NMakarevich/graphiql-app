8badf150b4406a69f9081d2965ce76822a369ad3
L1senochek <lisenochek96@gmail.com>
2024-09-06 19:34:45 +0300

    feat: add getSchema

--- src/app/graphiql/page.tsx
+++ src/app/graphiql/page.tsx
@@ -1,4 +1,64 @@
+'use client';
+
+import getSchema from '@/utils/graphQL/getSchema/getSchema';
+import { useEffect } from 'react';
+
+const endpoint2 = 'https://swapi-graphql.netlify.app/.netlify/functions/index';
+const endpoint3 = 'https://countries.trevorblades.com/';
+
+const query = `
+    query {
+      pokemon(name: "Pikachu") {
+        id
+        name
+        classification
+        types
+      }
+    }
+    `;
+
+const query2 = `
+    query {
+      allPeople {
+        people {
+          name
+          birthYear
+          species {
+            name
+          }
+        }
+      }
+    }
+    `;
+
+const query3 = `
+    query {
+      country(code: "AT") {
+        name
+        capital
+        currency
+        languages {
+          name
+        }
+      }
+    }
+    `;
+
 function GraphiQL(): JSX.Element {
+  const endpoint = 'https://graphql-pokemon2.vercel.app/';
+  useEffect((): void => {
+    (async (): Promise<void> => {
+      try {
+        const resultSchema = await getSchema(endpoint);
+        console.log(resultSchema);
+        console.group(endpoint2, endpoint3, query2, query3, query);
+      } catch (error) {
+        const errorMessage = String((error as Error).message);
+        console.error('getSchema Error:', error, errorMessage);
+      }
+    })();
+  }, []);
+
   return <div>GraphiQL</div>;
 }
 
